349a2996d9f7963b2e80554ec8ba4d0bc584216d
Shibayama <obzara@outlook.jp>
2016-04-24 05:20:02 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :articles
   
   root 'pages#home'
   get 'about', to: 'pages#about'
