d17da130adecb9c67d3b0f2fd0977823c6abc9f7
Shibayama <obzara@outlook.jp>
2016-04-27 06:17:19 +0000

    add association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
     validates :title, presence: true, length: {minimum: 3, maxmum: 50}
     validates :description, presence: true, length: {minimum: 10, maxmum: 300}
+    belongs_to :user
+    validates :user_id, presence: true
 end
