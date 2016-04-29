fefc9ff071bf12ef5b835407ec6bf43588a6cbe4
Shibayama <obzara@outlook.jp>
2016-04-29 16:24:39 +0000

    admin user sessting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
    
    before_action :set_user, only: [:edit, :update, :show]
-   before_action :require_same_user, only: [:edit, :update]
-   
+   before_action :require_same_user, only: [:edit, :update, :destroy]
+   before_action :require_admin, only: [:destroy]
    
    def index
       @users = User.paginate(page: params[:page], per_page: 5)
@@ -30,6 +30,13 @@ class UsersController < ApplicationController
    def edit
    end
    
+   def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      flash[:danger] = "User ans all articles created by user have been deleted"
+      redirect_to users_path
+   end
+   
    def update
       if @user.update(user_params)
          flash[:success] = "update success"
@@ -50,8 +57,16 @@ class UsersController < ApplicationController
    end
    
    def require_same_user
-      if current_user != @user
+      if current_user != @user and !current_user.admin?
          flash[:danger] = "you can only edit your own account"
+         redirect_to root_path
+      end
+   end
+   
+   
+   def require_admin
+      if logged_in? and !current_user.admin?
+         flash[:danger] = "only admin users can perform that action"
          redirect_to root_path
       end
    end
